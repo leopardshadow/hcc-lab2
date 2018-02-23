729c2706de6a1834c836642eadb337ec4be6a2db
yungshansu <michael1994822@gmail.com>
2018-02-23 13:04:51 +0800

    complete activity 1 answer

---
+++ catkin_ws/src/ros_example/src/node_example.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from sensor_msgs.msg import CompressedImage, Image
+from std_msgs.msg import String
+import rospy
+
+class RosExampleNode(object):
+    def __init__(self):
+        self.node_name = "ROS_Example_Node"
+
+        # Write Publishers
+        self.pub_image_info_string = rospy.Publisher("~image_info_string", String, queue_size=1)
+        # Write Subscribers
+        self.sub_image = rospy.Subscriber("/gcdog/camera_node/image/compressed", CompressedImage, self.cbImage, queue_size=1)
+       
+        rospy.loginfo("Initialized %s." %(self.node_name))
+
+    def cbImage(self,image_msg):
+	#Generate string message
+	image_info_string = "Blue yellow line"
+	#Publish string message
+	self.pub_image_info_string.publish(image_info_string)
+	
+
+
+if __name__ == '__main__': 
+    rospy.init_node('Ros_example_node',anonymous=False)
+    ros_node = RosExampleNode()
+    rospy.spin()
+
