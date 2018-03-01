e67c55e4c5d410d11f1ff99703eda69dcdb16b96
leopardshadow <thurtu811@gmail.com>
2018-03-01 01:25:48 -0500

    in-class task

--- catkin_ws/src/ros_example/src/node_example.py
+++ catkin_ws/src/ros_example/src/node_example.py
@@ -10,14 +10,15 @@ class RosExampleNode(object):
         # Write Publishers, please fill in XXX
         self.pub_image_info_string = rospy.Publisher("~image_info_string", String, queue_size=1)
         # Write Subscribers, please fill in XXX
-        self.sub_image = rospy.Subscriber("XXX", CompressedImage, "XXX", queue_size=1)
+        self.sub_image = rospy.Subscriber("~image", CompressedImage, self.cbImage, queue_size=1)
         rospy.loginfo("Initialized %s." %(self.node_name))
 
-    def cbImage(self,image_msg):
+    def cbImage(self, image_msg):
 	#Generate string message
 	image_info_string = "Blue yellow line"
+	print image_info_string
 	#Publish string message, please fill in XXX
-	self.pub_image_info_string."XXX"("XXX")
+	self.pub_image_info_string.publish(image_info_string)
 	
 
 
